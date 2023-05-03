4b02733f81127353d545d827ce3b8bf7cac2c4c2
sakib5452 <najmus191@gmail.com>
2023-05-03 12:32:54 +0600

    router set

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import AuthProvider from './providers/AuthProvider.jsx'
 import { RouterProvider } from 'react-router-dom'
+import router from './router/Routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
