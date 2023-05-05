d36d230959af4a28f2f9470c28768ea14836bb4e
sakib5452 <najmus191@gmail.com>
2023-05-05 17:16:53 +0600

    blog

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -24,9 +24,6 @@ const Blog = () => {
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">Tell us the differences between uncontrolled and controlled components</h2>
                         <p>In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
                     </div>
                 </div>
                 <div className="card w-96 bg-base-100 shadow-xl">
@@ -42,9 +39,7 @@ const Blog = () => {
                             PropTypes.string : The prop should be a string.
                             PropTypes.func : The prop should be a function.
                             PropTypes.array : The prop should be an array.</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
+
                     </div>
                 </div>
                 <div className="card w-96 bg-base-100 shadow-xl">
@@ -54,9 +49,7 @@ const Blog = () => {
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">What is a custom hook, and why will you create a custom hook?</h2>
                         <p>Custom React JS hooks offer reusability as when a custom hook is created, it can be reused easily, which makes the code cleaner and reduces the time to write the code. It also enhances the rendering speed of the code as a custom hook does not need to be rendered again and again while rendering the whole code.</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
+
                     </div>
                 </div>
                 <div className="card w-96 bg-base-100 shadow-xl">
@@ -66,9 +59,7 @@ const Blog = () => {
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">Tell us the difference between nodejs and express js.</h2>
                         <p>js web application framework that provides a robust set of features for web and mobile applications. In other words, NodeJS is the package, which provides the JavaScript run-time environment, whereas Express is a framework that sits on top of NodeJS and helps us to handle requests and responses.</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
+
                     </div>
                 </div>
 
