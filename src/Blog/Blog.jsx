31a3f7fc0bd2e19c40029efa7690f54946df9e85
sakib5452 <najmus191@gmail.com>
2023-05-05 15:44:42 +0600

    blog

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -1,10 +1,78 @@
 import React from 'react';
 import Footer from '../pages/Shared/Footer/Footer';
+import pic from '../../src/assets/controlled-and-uncontrolled-components-in-react-thumbnail.webp'
+import pic1 from '../../src/assets/images (1).jpg'
+import pic2 from '../../src/assets/Implementing-React-Custom-Hooks-A-Complete-Guide.png'
+import pic3 from '../../src/assets/Express-JS-vs-Node-JS_-Why-its-Time-to-Migrate_-1.png.webp'
+import Pdf from "react-to-pdf";
+const ref = React.createRef();
 
 const Blog = () => {
     return (
         <div>
+            <div className='ml-10 mt-10'>
+                <Pdf targetRef={ref} filename="code-example.pdf">
+                    {({ toPdf }) => <button className='btn btn-primary' onClick={toPdf}>Download Pdf</button>}
+                </Pdf>
+            </div>
+            <div className='grid grid-cols-2 gap-20 ml-60 mr-40 mt-10 mb-10' ref={ref} >
 
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src={pic} alt="Shoes" className="rounded-xl h-64" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Tell us the differences between uncontrolled and controlled components</h2>
+                        <p>In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src={pic1} alt="Shoes" className="rounded-xl h-64" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">How to validate React props using PropTypes.</h2>
+                        <p>React PropTypes validators
+                            PropTypes.any : The prop can be of any data type.
+                            PropTypes.bool : The prop should be a Boolean.
+                            PropTypes.number : The prop should be a number.
+                            PropTypes.string : The prop should be a string.
+                            PropTypes.func : The prop should be a function.
+                            PropTypes.array : The prop should be an array.</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src={pic2} alt="Shoes" className="rounded-xl h-64" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">What is a custom hook, and why will you create a custom hook?</h2>
+                        <p>Custom React JS hooks offer reusability as when a custom hook is created, it can be reused easily, which makes the code cleaner and reduces the time to write the code. It also enhances the rendering speed of the code as a custom hook does not need to be rendered again and again while rendering the whole code.</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src={pic3} alt="Shoes" className="rounded-xl h-64" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Tell us the difference between nodejs and express js.</h2>
+                        <p>js web application framework that provides a robust set of features for web and mobile applications. In other words, NodeJS is the package, which provides the JavaScript run-time environment, whereas Express is a framework that sits on top of NodeJS and helps us to handle requests and responses.</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
             <Footer></Footer>
         </div>
 
--- src/Recipes/Recipes.jsx
+++ src/Recipes/Recipes.jsx
@@ -3,35 +3,77 @@ import React from 'react';
 const Recipes = () => {
 
     return (
-        <div className='ml-80 mt-10 mb-10'>
-            <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
-                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-mono text-5xl">
-                        <span style={{ "--value": 15 }}></span>
-                    </span>
-                    days
-                </div>
-                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-mono text-5xl">
-                        <span style={{ "--value": 10 }}></span>
-                    </span>
-                    hours
-                </div>
-                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-mono text-5xl">
-                        <span style={{ "--value": 24 }}></span>
-                    </span>
-                    min
-                </div>
-                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-mono text-5xl">
-                        <span style={{ "--value": 53 }}></span>
-                    </span>
-                    sec
+        <div>
+            <h1 className='mt-10 text-center text-3xl font-bold'>Client Review</h1>
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                <div className="grid gap-10 mx-auto sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg">
+                    <div>
+                        <img
+                            className="object-cover w-24 h-24 rounded-full shadow"
+                            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                            alt="Person"
+                        />
+                        <div className="flex flex-col justify-center mt-2">
+                            <p className="text-lg font-bold">Oliver Aguilerra</p>
+                            <p className="mb-4 text-xs text-gray-800">Product Manager</p>
+                            <p className="text-sm tracking-wide text-gray-800">
+                                Pommy ipsum bent as a nine bob note naff off biscuits nowt, a
+                                cuppa unhand me sir hadn't done it in donkey's years sod's law.
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <img
+                            className="object-cover w-24 h-24 rounded-full shadow"
+                            src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                            alt="Person"
+                        />
+                        <div className="flex flex-col justify-center mt-2">
+                            <p className="text-lg font-bold">Marta Clermont</p>
+                            <p className="mb-4 text-xs text-gray-800">Design Team Lead</p>
+                            <p className="text-sm tracking-wide text-gray-800">
+                                Secondary fermentation degrees plato units of bitterness, cask
+                                conditioned ale ibu real ale pint glass craft beer. krausen goblet
+                                grainy ibu.
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <img
+                            className="object-cover w-24 h-24 rounded-full shadow"
+                            src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                            alt="Person"
+                        />
+                        <div className="flex flex-col justify-center mt-2">
+                            <p className="text-lg font-bold">Alice Melbourne</p>
+                            <p className="mb-4 text-xs text-gray-800">Human Resources</p>
+                            <p className="text-sm tracking-wide text-gray-800">
+                                I just closed my eyes and in a nanosecond I cured myself from this
+                                ridiculous model of disease, addiction and obsession.
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <img
+                            className="object-cover w-24 h-24 rounded-full shadow"
+                            src="https://images.pexels.com/photos/3931603/pexels-photo-3931603.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                            alt="Person"
+                        />
+                        <div className="flex flex-col justify-center mt-2">
+                            <p className="text-lg font-bold">Martin Garix Potter</p>
+                            <p className="mb-4 text-xs text-gray-800">Good guy</p>
+                            <p className="text-sm tracking-wide text-gray-800">
+                                Est Schlitz shoreditch fashion axe. Messenger bag cupidatat
+                                Williamsburg sustainable aliqua, umami shabby chic artisan duis
+                                pickled.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     );
 };
+
 
 export default Recipes;
